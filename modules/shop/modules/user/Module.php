eb9d86d651222caeb96074e07f17835e920402f6
Thinking Song <123wuming_123@163.com>
2016-03-21 21:14:51 +0800

    add shop/user module

---
+++ modules/shop/modules/user/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\shop\modules\user;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\shop\modules\user\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
--- modules/shop/modules/user/views/default/index.php
+++ modules/shop/modules/user/views/default/index.php
@@ -1,4 +1,4 @@
-<div class="shop-default-index">
+<div class="user-default-index">
     <h1><?= $this->context->action->uniqueId ?></h1>
     <p>
         This is the view content for action "<?= $this->context->action->id ?>".
