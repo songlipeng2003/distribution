a4c701e928d54adbdb1699a2c75b04cbdb3a2187
Thinking Song <123wuming_123@163.com>
2016-04-11 18:51:52 +0800

    add show pay number

--- modules/shop/modules/user/views/order/pay.php
+++ modules/shop/modules/user/views/order/pay.php
@@ -11,6 +11,10 @@ $this->title = '订单支付';
   <h1 class="am-header-title"><?= Html::encode($this->title) ?></h1>
 </header>
 
+<div style="font-size: 24px; text-align: center; padding: 100px 5px;">
+    支付金额：<?= $model->totalAmount ?>元
+</div>
+
 <?php 
 $this->registerJsFile('/js/pingpp.js');
 ?>
