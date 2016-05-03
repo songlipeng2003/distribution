2440578819a3fac64f16834872b3788279aa524f
Thinking Song <123wuming_123@163.com>
2016-05-04 00:17:08 +0800

    update news

--- modules/shop/views/news/view.php
+++ modules/shop/views/news/view.php
@@ -1,4 +1,7 @@
 <?php
+use app\modules\weixin\models\Weixin;
+
+$js = Weixin::getApplication()->js;
 ?>
 <!DOCTYPE html>
 <html>
@@ -6,18 +9,37 @@
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=0">
     <title><?= Yii::$app->settings->get('system', 'weixinName') ?></title>
+    <script src="https://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+    <script type="text/javascript">
+        wx.config(<?php echo $js->config(array('closeWindow'), false) ?>);
+        wx.onMenuShareTimeline({
+            title: '<?= $model->title ?>'
+        });
+        wx.onMenuShareAppMessage({
+            title: '<?= $model->title ?>'
+        });
+        wx.onMenuShareQQ({
+            title: '<?= $model->title ?>'
+        });
+        wx.onMenuShareWeibo({
+            title: '<?= $model->title ?>'
+        });
+    </script>
     <style type="text/css" media="screen">
         body {
             max-width: 740px;
             margin: 20px auto;
         }
+        .page {
+            padding: 20px 15px 15px;
+        }
         h1{
-            padding-bottom: 10px;
-            margin-bottom: 14px;
-            border-bottom: 1px solid #e7e7eb;
+            margin-bottom: 10px;
+            font-size: 24px;
         }
         .info {
             margin-bottom: 18px;
+            word-wrap: break-word;
         }
         .date, .author {
             color: #8c8c8c;
@@ -30,17 +52,23 @@
             color: #607fa6;
             font-size: 16px;
         }
+        .content {
+            word-wrap: break-word;
+            overflow: hidden;
+        }
     </style>
 </head>
 <body>
-    <h1><?= $model->title ?></h1>
-    <div class="info">
-        <span class="date"><?= substr($model->createdAt, 0, 10) ?></span>
-        <span class="author"><?= $model->author ?></span>
-        <a><?= Yii::$app->settings->get('system', 'weixinName') ?></a>
-    </div>
-    <div>
-        <?= $model->content ?>
+    <div class="page">
+        <h1><?= $model->title ?></h1>
+        <div class="info">
+            <span class="date"><?= substr($model->createdAt, 0, 10) ?></span>
+            <span class="author"><?= $model->author ?></span>
+            <a><?= Yii::$app->settings->get('system', 'weixinName') ?></a>
+        </div>
+        <div class="content">
+            <?= $model->content ?>
+        </div>
     </div>
 </body>
 </html>
